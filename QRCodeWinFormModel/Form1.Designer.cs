46baea11de7fa1da33054e3603eb5a7a3f4d7aa0
Dustray <yinxiyue1995@outlook.com>
2017-12-29 15:59:05 +0800

    good over

---
+++ QRCodeWinFormModel/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace QRCodeWinFormModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.showPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.showPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(250, 12);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(79, 26);
+            this.submitBtn.TabIndex = 0;
+            this.submitBtn.Text = "生  成";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // messageText
+            // 
+            this.messageText.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.messageText.Location = new System.Drawing.Point(12, 12);
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(232, 26);
+            this.messageText.TabIndex = 1;
+            // 
+            // showPic
+            // 
+            this.showPic.Location = new System.Drawing.Point(12, 44);
+            this.showPic.Name = "showPic";
+            this.showPic.Size = new System.Drawing.Size(317, 317);
+            this.showPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.showPic.TabIndex = 2;
+            this.showPic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 373);
+            this.Controls.Add(this.showPic);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.submitBtn);
+            this.MaximumSize = new System.Drawing.Size(357, 412);
+            this.MinimumSize = new System.Drawing.Size(357, 412);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.showPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.PictureBox showPic;
+    }
+}
+
---
+++ QRCodeWinFormModel/Form1.cs
@@ -0,0 +1,42 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeWinFormModel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Bitmap generateQRCode(string msg)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(msg, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return qrCodeImage;
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            if (messageText.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                showPic.Image=generateQRCode(messageText.Text);
+            }
+        }
+    }
+}
