bd88b1bbe8dc1eb629e896a00153b574de386d24
Dustray <yinxiyue1995@outlook.com>
2017-12-29 16:46:51 +0800

    自定义简易样式

--- QRCodeWinFormModel/Form1.cs
+++ QRCodeWinFormModel/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Win32二维码程序模型
 namespace QRCodeWinFormModel
 {
     public partial class Form1 : Form
@@ -23,10 +24,9 @@ namespace QRCodeWinFormModel
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(msg, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(qrCodeData);
-            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.DarkRed, Color.PaleGreen, (Bitmap)Bitmap.FromFile(@"C:\Users\yinxi\OneDrive\图标\Win10表情包Emoji_IT之家.zip\006maLokjw1f2nqnpfa6bg308w08wb2a.gif.ifl\IMG00122.bmp"),25,1,true);
             return qrCodeImage;
         }
-
         private void submitBtn_Click(object sender, EventArgs e)
         {
             if (messageText.Text == "")
